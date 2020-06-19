1e5a358f43d8527baa77e7c94c578bfa5e21da80
shobhit-coder <shobhit1998@yahoo.co.in>
2020-06-19 13:26:58 +0530

    added windows support

--- mac_notify.py
+++ mac_notify.py
@@ -3,13 +3,22 @@ import time
 import os
 import multiprocessing
 import notifiy_mgr
+from win10toast import ToastNotifier
 
 notification_queue = multiprocessing.Queue()
 
 def notify(title, text):
-    os.system("""
-              osascript -e 'display notification "{}" with title "{}"'
-              """.format(text, title))
+    if os.name=="nt":
+        toaster = ToastNotifier()
+        toaster.show_toast(title,text,
+                    icon_path=None,
+                    duration=5,
+                    threaded=True)
+        
+    else:          ##TODO: add elif for macOS
+        os.system("""
+               osascript -e 'display notification "{}" with title "{}"'
+               """.format(text, title))
 
 if __name__ == "__main__":
     background_process = multiprocessing.Process(target=notifiy_mgr.notify_from_queue)
